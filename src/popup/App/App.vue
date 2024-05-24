826a9668b65c3595d43848e34a066bac04622182
noah227 <noah227@foxmail.com>
2024-05-24 16:31:20 +0800

    chore: 简化popup页面

--- src/popup/App/App.vue
+++ src/popup/App/App.vue
@@ -1,9 +1,6 @@
 <template>
     <div>
-        <div>this is popup page</div>
         <img src="@/assets/logo.png" alt="">
-        <div>popup的输出在自己的世界里，对着popup的弹出右键可以审查元素。</div>
-        <div>popup has it's isolated running world, right click to trigger context menu and inspect.</div>
     </div>
 </template>
 
